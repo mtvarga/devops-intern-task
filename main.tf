28ea9c6d366cd89b49643d15db84ad1e5c8776d9
Máté Varga <varga.mate426@gmail.com>
2022-03-29 00:15:54 +0200

    Optimized for GitHub Actions

--- main.tf
+++ main.tf
@@ -3,53 +3,53 @@ resource "azurerm_resource_group" "rg" {
     location = "westeurope"
 }
 
-resource "azurerm_virtual_network" "tfvnet" {
-    name = "virtual_network"
+resource "azurerm_virtual_network" "tfvm_vnet" {
+    name = "tfvm_vnet"
     address_space = ["10.0.0.0/16"]
     location = azurerm_resource_group.rg.location
     resource_group_name = azurerm_resource_group.rg.name
 }
 
-resource "azurerm_subnet" "tfsubnet" {
-    name = "subnet"
+resource "azurerm_subnet" "tfvm_subnet" {
+    name = "tfvm_subnet"
     resource_group_name = azurerm_resource_group.rg.name
-    virtual_network_name = azurerm_virtual_network.tfvnet.name
+    virtual_network_name = azurerm_virtual_network.tfvm_vnet.name
     address_prefixes = ["10.0.1.0/24"]
 }
 
-resource "azurerm_public_ip" "tfpublicip" {
-    name = "public_ip"
+resource "azurerm_public_ip" "tfvm_public_ip" {
+    name = "tfvm_public_ip"
     location = azurerm_resource_group.rg.location
     resource_group_name = azurerm_resource_group.rg.name
     allocation_method = "Dynamic"
 }
 
-resource "azurerm_network_interface" "tfnic" {
-    name = "nic"
+resource "azurerm_network_interface" "tfvm_nic" {
+    name = "tfvm_nic"
     location = azurerm_resource_group.rg.location
     resource_group_name = azurerm_resource_group.rg.name
 
     ip_configuration {
-        name = "default_ip_config"
-        subnet_id = azurerm_subnet.tfsubnet.id
+        name = "tfvm_default_ip_config"
+        subnet_id = azurerm_subnet.tfvm_subnet.id
         private_ip_address_allocation = "Dynamic"
-        public_ip_address_id = azurerm_public_ip.tfpublicip.id
+        public_ip_address_id = azurerm_public_ip.tfvm_public_ip.id
     }
 }
 
 resource "azurerm_linux_virtual_machine" "tfvm" {
-    name = "terraform_vm"
+    name = "tfvm"
     location = azurerm_resource_group.rg.location
     resource_group_name = azurerm_resource_group.rg.name
-    network_interface_ids = [azurerm_network_interface.tfnic.id]
+    network_interface_ids = [azurerm_network_interface.tfvm_nic.id]
     size = "Standard_B1s"
-    admin_username = "adminuser"
+    admin_username = "azure_admin"
     admin_password = "VerySecureAdminPassword1234"
     disable_password_authentication = false
     computer_name = "tfvm"
 
     os_disk {
-        name = "os_disk"
+        name = "tfvm_os_disk"
         caching = "ReadWrite"
         storage_account_type = "Standard_LRS"
     }
--- providers.tf
+++ providers.tf
@@ -1,15 +1,21 @@
 terraform {
+    required_version = ">=0.12"
 
-  required_version = ">=0.12"
-
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = "~>2.0"
+    required_providers {
+        azurerm = {
+            source = "hashicorp/azurerm"
+            version = "~>2.0"
+        }
     }
-  }
+    
+    backend "azurerm" {
+        resource_group_name = "devops-terraform-intern-task"
+        storage_account_name = "devopsinterntfstatestore"
+        container_name = "tfvmstates"
+        key = "tfvm.tfstate"
+    }
 }
 
 provider "azurerm" {
-  features {}
+    features {}
 }
